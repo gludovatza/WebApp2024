77b7a9acc0074a85328499927e8ce00395c8f93c
Gludovatz Attila <attila@gludovatz.hu>
2025-01-21 08:58:43 +0100

    18. fejezet

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp2024.Models
+{
+    public class Category
+    {
+        public long CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public IEnumerable<Product>? Products { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using WebApp2024.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opts => {
+    opts.UseSqlServer(builder.Configuration[
+    "ConnectionStrings:ProductConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+var app = builder.Build();
+app.UseMiddleware<WebApp2024.TestMiddleware>();
 app.MapGet("/", () => "Hello World!");
+
+var context = app.Services.CreateScope().ServiceProvider
+ .GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
 
 app.Run();

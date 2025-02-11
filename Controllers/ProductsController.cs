5f0774a1a831e7218bb6bb341257a91ad19a85cc
Gludovatz Attila <attila@gludovatz.hu>
2025-02-11 08:08:49 +0100

    Applying the API Controller Attribute

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using WebApp2024.Models;
 
 namespace WebApp2024.Controllers
 {
+    [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
@@ -28,20 +29,16 @@ namespace WebApp2024.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> SaveProduct([FromBody] ProductBindingTarget target)
+        public async Task<IActionResult> SaveProduct(ProductBindingTarget target)
         {
-            if (ModelState.IsValid)
-            {
-                Product p = target.ToProduct();
-                await context.Products.AddAsync(p);
-                await context.SaveChangesAsync();
-                return Ok(p);
-            }
-            return BadRequest(ModelState);
+            Product p = target.ToProduct();
+            await context.Products.AddAsync(p);
+            await context.SaveChangesAsync();
+            return Ok(p);
         }
 
         [HttpPut]
-        public async Task UpdateProduct([FromBody] Product product)
+        public async Task UpdateProduct(Product product)
         {
             context.Products.Update(product);
             await context.SaveChangesAsync();
